a188ff845689c3154e4998e7274a7299831e2b86
miku1001 <pauloted.22@gmail.com>
2026-02-12 19:28:17 +0800

    optimizing embedding

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
 
 
 #grab api key
@@ -33,29 +35,34 @@ chunks = text_splitter.split_documents(documents)
 
 #embedding and store
 embeddings = HuggingFaceEmbeddings(
-  model_name="sentence-transformers/all-MiniLM-L6-v2"
+  model_name="sentence-transformers/all-MiniLM-L6-v2",
+  encode_kwargs={'normalize_embeddings': True}
   )
 
 store = Chroma.from_documents(documents=chunks, embedding=embeddings)
 retriever = store.as_retriever()
 
 #prompt template
-prompt = PromptTemplate(
-          template="""Answer the question based only on the following context: {context}. 
-          Reject questions unrelated to the context: {context}.
-          Note: Be accurate and concise. Just answer the user's chat.
-
-          Question: {question}
-          """)
+prompt = ChatPromptTemplate.from_messages([
+   ("system", """You are a legal assistant.
+      Answer the question strictly using ONLY the provided context.
+      If the answer is not found in the context, say:
+      "The answer is not found in the provided document."
+      Be accurate and concise."""),
+    ("user", "Question: {question}\nContext: {context}")
+])
 
 question = input('Enter question: ')
 
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
+docs = retriever.invoke(question)
+docs_content = "\n\n".join(doc.page_content for doc in docs)
 
-chain = {"context": retriever | format_docs, "question": RunnablePassthrough() }| prompt | model
+chain = prompt | model |StrOutputParser()
 
-print(chain.invoke(question).content)
+print(chain.invoke({
+   "question":question,
+   "context": docs_content
+}))
 
 
 
